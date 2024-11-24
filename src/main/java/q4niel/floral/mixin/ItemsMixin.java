0148e19ea2f5e0c3a299183901e18c5c51b2783f
q4niel <github.obscure938@passmail.net>
2024-11-24 13:26:02 +0100

    Regular, Splash, Lingering Potions & Glass Bottle Stack MaxCount set to 16

---
+++ src/main/java/q4niel/floral/mixin/ItemsMixin.java
@@ -0,0 +1,55 @@
+package q4niel.floral.mixin;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(Items.class)
+public class ItemsMixin {
+    @ModifyArg (
+            method = "<clinit>",
+            at = @At (
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/GlassBottleItem;<init>(Lnet/minecraft/item/Item$Settings;)V"
+            ),
+            index = 0
+    )
+    private static Item.Settings GlassBottleItem(Item.Settings settings) {
+        return settings.maxCount(16);
+    }
+
+    @ModifyArg (
+            method = "<clinit>",
+            at = @At (
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;",
+                    ordinal = 97 // Potion
+            ),
+            index = 0
+    )
+    private static int potionCount(int maxCount) { return 16; }
+
+    @ModifyArg (
+            method = "<clinit>",
+            at = @At (
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;",
+                    ordinal = 125 // Splash Potion
+            ),
+            index = 0
+    )
+    private static int splashCount(int maxCount) { return 16; }
+
+    @ModifyArg (
+            method = "<clinit>",
+            at = @At (
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;",
+                    ordinal = 126 // Lingering Potion
+            ),
+            index = 0
+    )
+    private static int lingeringCount(int maxCount) { return 16; }
+}
