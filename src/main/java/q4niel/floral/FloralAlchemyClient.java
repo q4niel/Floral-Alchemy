5ffb34a7e50169b79582f1fe87b5710bf56973f2
q4niel <github.obscure938@passmail.net>
2024-11-24 12:49:54 +0100

    Fabric Template Mod

---
+++ src/main/java/q4niel/floral/FloralAlchemyClient.java
@@ -0,0 +1,10 @@
+package q4niel.floral;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FloralAlchemyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/q4niel/floral/FloralAlchemyDataGenerator.java
@@ -0,0 +1,11 @@
+package q4niel.floral;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FloralAlchemyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
